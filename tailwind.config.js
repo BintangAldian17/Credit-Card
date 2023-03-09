cf4659aa3d6f353b6d2f261df8bb4577618bb58e
BintangAldian17 <bintangaldian17@gmail.com>
2023-03-09 18:33:46 +0700

    add and styling card

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'left-content': "url('./dist/images/bg-main-desktop.png')",
+      },
+      fontFamily: {
+        'primary': ['Space Grotesk'],
+      },
+    },
   },
   plugins: [],
 }
